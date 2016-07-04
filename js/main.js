e547f432d7844fe973bbba133da06b1e68d144cd
Kevin <kevin.bouwmeester@gmail.com>
2016-07-04 12:02:47 +0200

    Add subscription code

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ if('serviceWorker' in navigator) {
   console.log('Service Worker is supported');
   navigator.serviceWorker.register('sw.js').then(function(reg) {
     console.log(':^',reg);
-    // TODO
+    // NEW
+    reg.pushManager.subscribe({
+      userVisibleOnly: true
+    }).then(function(sub) {
+      console.log('endpoint:', sub.endpoint);
+    });
+    // END
   }).catch(function(err) {
     console.log(':^', err);
   });
